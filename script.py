01e86f61062024561a63a98861da77869e43072a
Kawayan <kawayan.deguzman@gmail.com>
2023-07-04 16:24:38 +0800

    add comments

--- script.py
+++ script.py
@@ -10,6 +10,8 @@ response = requests.get(url)
 
 print(api_key)
 
+# call API and get information about the rain, weather, amount of rainfall
+
 if response.status_code == 200:
     data = response.json()
     temp = data['main']['temp']
@@ -18,3 +20,6 @@ if response.status_code == 200:
     print(f'Description: {desc}')
 else:
     print('Error fetching weather data')
+
+# if there is rain or a certain amount of rainfall send an email or text to me somehow? 
+# I think i would need another API for this
