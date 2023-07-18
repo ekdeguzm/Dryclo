169776b75a5f3a20252e8407cc7634224d2b08b1
Kawayan <kawayan.deguzman@gmail.com>
2023-07-18 17:16:27 +0800

    import keys

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests
 from twilio.rest import Client
+import keys
 
 client = Client(keys.account_sid, keys.auth_token)
 
+message = client.messages.create(
+    body="This is a test message!",
+    from_ = keys.twilio_number,
+    to = keys.target_number 
+)
+
+print(message.body)
 
 api_key = '04d30ff35dffb5923aa11546c2059bfc'
 
