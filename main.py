5de5d76be26a2b4a62a06e70501787ac486b7aa8
Kawayan <kawayan.deguzman@gmail.com>
2023-07-18 18:08:11 +0800

    update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import keys
 client = Client(keys.account_sid, keys.auth_token)
 
 message = client.messages.create(
-    body="This is a test message!",
+    body = "This is a test message!",
     from_ = keys.twilio_number,
     to = keys.target_number 
 )
